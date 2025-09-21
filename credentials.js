021e790b2a220dd5844325f6f24f02a10afac2bd
chambies2015 <65683899+chambies2015@users.noreply.github.com>
2025-09-21 16:11:05 -0500

    added movie posters

---
+++ credentials.js
@@ -0,0 +1,3 @@
+// this is where you'll put your omdb api key to grab movie posters.
+// if no key is present, the program will still work, but the posters will be missing.
+const OMDB_API_KEY = '';
