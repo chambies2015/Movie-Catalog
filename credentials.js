0abe0d50bdf5a9d0f63594a166deabbce6fae69e
chambies2015 <65683899+chambies2015@users.noreply.github.com>
2025-09-21 16:15:22 -0500

    stop tracking credentials.js

--- credentials.js
+++
@@ -1,3 +0,0 @@
-// this is where you'll put your omdb api key to grab movie posters.
-// if no key is present, the program will still work, but the posters will be missing.
-const OMDB_API_KEY = '';
